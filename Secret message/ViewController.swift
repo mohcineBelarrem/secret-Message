bd714c3ddfa203bd5fb92e967c953ce0505aec19
Mohcine Belarrem <belarrem@idm.fr>
2015-07-29 16:53:08 +0200

    Initial Commit

---
+++ Secret message/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Secret message
+//
+//  Created by mohcine  on 7/29/15.
+//  Copyright (c) 2015 mohcine . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
